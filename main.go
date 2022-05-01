5d90266ee0bc17982e732c90702b46c291d228b2
b1018043 <grey.fox.0417@gmail.com>
2022-05-01 16:44:21 +0900

    run ebiten tutorial

--- main.go
+++ main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type Game struct{}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.RGBA{0xff, 0, 0, 0xff})
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+var _ ebiten.Game = &Game{}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Fill")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatalln(err)
+	}
+}
