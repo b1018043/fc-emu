0ee398f95b073131182e6925650cf121d7756c39
b1018043 <grey.fox.0417@gmail.com>
2022-05-03 16:13:27 +0900

    feat:add ope 80~af

--- pkg/cpu/cpu.go
+++ pkg/cpu/cpu.go
@@ -36,12 +36,14 @@ var operation_names = [256]string{
 	"JSR", "AND", "NONE", "NONE", "BIT", "AND", "ROL", "NONE", "PLP", "AND", "POL", "NONE", "BIT", "AND", "ROL", "NONE",
 	"BMI", "AND", "NONE", "NONE", "NONE", "AND", "ROL", "NONE", "SEC", "AND", "NONE", "NONE", "NONE", "AND", "ROL", "NONE",
 	"RTI", "EOR", "NONE", "NONE", "NONE", "EOR", "LSR", "NONE", "PHA", "EOR", "LSR", "NONE", "JMP", "EOR", "LSR", "NONE",
-	"BVC", "EOR", "NONE", "NONE", "NONE", "EOR", "LSR", "NONE",
-	"CLI", "EOR", "NONE", "NONE", "NONE", "EOR", "LSR", "NONE",
-	"RTS", "ADC", "NONE", "NONE", "NONE", "ADC", "ROR", "NONE",
-	"PLA", "ADC", "ROR", "NONE", "JMP", "ADC", "ROR", "NONE",
-	"BVS", "ADC", "NONE", "NONE", "NONE", "ADC", "ROR", "NONE",
-	"SEI", "ADC", "NONE", "NONE", "NONE", "ADC", "ROR", "NONE",
+	"BVC", "EOR", "NONE", "NONE", "NONE", "EOR", "LSR", "NONE", "CLI", "EOR", "NONE", "NONE", "NONE", "EOR", "LSR", "NONE",
+	"RTS", "ADC", "NONE", "NONE", "NONE", "ADC", "ROR", "NONE", "PLA", "ADC", "ROR", "NONE", "JMP", "ADC", "ROR", "NONE",
+	"BVS", "ADC", "NONE", "NONE", "NONE", "ADC", "ROR", "NONE", "SEI", "ADC", "NONE", "NONE", "NONE", "ADC", "ROR", "NONE",
+	"NONE", "STA", "NONE", "NONE", "STY", "STA", "STX", "NONE", "DEY", "NONE", "TXA", "NONE", "STY", "STA", "STX", "NONE",
+	"BCC", "STA", "NONE", "NONE", "STY", "STA", "STX", "NONE",
+	"TYA", "STA", "TXS", "NONE", "NONE", "STA", "NONE", "NONE",
+	"LDY", "LDA", "LDX", "NONE", "LDY", "LDA", "LDX", "NONE",
+	"TAY", "LDA", "TAX", "NONE", "LDY", "LDA", "LDX", "NONE",
 }
 
 var operation_sizes = [256]int{
@@ -50,12 +52,14 @@ var operation_sizes = [256]int{
 	3, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
 	2, 2, 0, 0, 0, 2, 2, 0, 1, 3, 0, 0, 0, 3, 3, 0,
 	1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
-	2, 2, 0, 0, 0, 2, 2, 0,
-	1, 3, 0, 0, 0, 3, 3, 0,
-	1, 2, 0, 0, 0, 2, 2, 0,
+	2, 2, 0, 0, 0, 2, 2, 0, 1, 3, 0, 0, 0, 3, 3, 0,
+	1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 1, 0, 3, 3, 3, 0,
+	2, 2, 0, 0, 0, 2, 2, 0, 1, 3, 0, 0, 0, 3, 3, 0,
+	0, 2, 0, 0, 2, 2, 2, 0, 1, 0, 1, 0, 3, 3, 3, 0,
+	2, 2, 0, 0, 2, 2, 2, 0,
+	1, 3, 1, 0, 0, 3, 0, 0,
+	2, 2, 2, 0, 2, 2, 2, 0,
 	1, 2, 1, 0, 3, 3, 3, 0,
-	2, 2, 0, 0, 0, 2, 2, 0,
-	1, 3, 0, 0, 0, 3, 3, 0,
 }
 
 var operation_modes = [256]int{
@@ -64,12 +68,14 @@ var operation_modes = [256]int{
 	modeAbsolute, modeIndirectX, 0, 0, modeZeroPage, modeZeroPage, modeZeroPage, 0, modeImplied, modeImmediate, modeAccumulator, 0, modeAbsolute, modeAbsolute, modeAbsolute, 0,
 	modeRelative, modeIndirectY, 0, 0, 0, modeZeroPageX, modeZeroPageX, 0, modeImplied, modeAbsoluteY, 0, 0, 0, modeAbsoluteX, modeAbsoluteX, 0,
 	modeImplied, modeIndirectX, 0, 0, 0, modeZeroPage, modeZeroPage, 0, modeImplied, modeImmediate, modeAccumulator, 0, modeAbsolute, modeAbsolute, modeAbsolute, 0,
-	modeRelative, modeIndirectY, 0, 0, 0, modeZeroPageX, modeZeroPageX, 0,
-	modeImplied, modeAbsoluteY, 0, 0, 0, modeAbsoluteX, modeAbsoluteX, 0,
-	modeImplied, modeIndirectX, 0, 0, 0, modeZeroPage, modeZeroPage, 0,
-	modeImplied, modeImmediate, modeAccumulator, 0, modeIndirect, modeAbsolute, modeAbsolute, 0,
-	modeRelative, modeIndirectY, 0, 0, 0, modeZeroPageX, modeZeroPageX, 0,
-	modeImplied, modeAbsoluteY, 0, 0, 0, modeAbsoluteX, modeAbsoluteX, 0,
+	modeRelative, modeIndirectY, 0, 0, 0, modeZeroPageX, modeZeroPageX, 0, modeImplied, modeAbsoluteY, 0, 0, 0, modeAbsoluteX, modeAbsoluteX, 0,
+	modeImplied, modeIndirectX, 0, 0, 0, modeZeroPage, modeZeroPage, 0, modeImplied, modeImmediate, modeAccumulator, 0, modeIndirect, modeAbsolute, modeAbsolute, 0,
+	modeRelative, modeIndirectY, 0, 0, 0, modeZeroPageX, modeZeroPageX, 0, modeImplied, modeAbsoluteY, 0, 0, 0, modeAbsoluteX, modeAbsoluteX, 0,
+	0, modeIndirectX, 0, 0, modeZeroPage, modeZeroPage, modeZeroPage, 0, modeImplied, 0, modeImplied, 0, modeAbsolute, modeAbsolute, modeAbsolute, 0,
+	modeRelative, modeIndirectY, 0, 0, modeZeroPageX, modeZeroPageX, modeZeroPageY, 0,
+	modeImplied, modeAbsoluteY, modeImplied, 0, 0, modeAbsoluteX, 0, 0,
+	modeImmediate, modeIndirectX, modeImmediate, 0, modeZeroPage, modeZeroPage, modeZeroPage, 0,
+	modeImplied, modeImmediate, modeImplied, 0, modeAbsolute, modeAbsolute, modeAbsolute, 0,
 }
 
 var operation_cycles = [256]int{
@@ -78,12 +84,14 @@ var operation_cycles = [256]int{
 	6, 6, 0, 0, 3, 3, 5, 0, 4, 2, 2, 0, 4, 4, 6, 0,
 	2, 5, 0, 0, 0, 4, 6, 0, 2, 4, 0, 0, 0, 4, 7, 0,
 	6, 6, 0, 0, 0, 3, 5, 0, 3, 2, 2, 0, 3, 4, 6, 0,
-	2, 5, 0, 0, 0, 4, 6, 0,
-	2, 4, 0, 0, 0, 4, 7, 0,
-	6, 6, 0, 0, 0, 3, 5, 0,
-	4, 2, 2, 0, 5, 4, 6, 0,
-	2, 5, 0, 0, 0, 4, 6, 0,
-	2, 4, 0, 0, 0, 4, 7, 0,
+	2, 5, 0, 0, 0, 4, 6, 0, 2, 4, 0, 0, 0, 4, 7, 0,
+	6, 6, 0, 0, 0, 3, 5, 0, 4, 2, 2, 0, 5, 4, 6, 0,
+	2, 5, 0, 0, 0, 4, 6, 0, 2, 4, 0, 0, 0, 4, 7, 0,
+	0, 6, 0, 0, 3, 3, 3, 0, 2, 0, 2, 0, 4, 4, 4, 0,
+	2, 6, 0, 0, 4, 4, 4, 0,
+	2, 5, 2, 0, 0, 5, 0, 0,
+	2, 6, 2, 0, 3, 3, 3, 0,
+	2, 2, 2, 0, 4, 4, 4, 0,
 }
 
 type CPU struct {
